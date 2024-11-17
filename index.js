c65b44ce349268aa4fdf67b09ecb264b20c9fe64
Akshat Gupta <akshatgupta0401@gmail.com>
2024-11-17 23:02:16 +0530

    completed the backend code

--- index.js
+++ index.js
@@ -1,1 +1,170 @@
 const express=require('express')
+const jwt=require('jsonwebtoken')
+const path=require('path')
+
+const app=express()
+app.use(express.json())
+
+const users=[]
+const todos=[]
+
+const JWT_SECRET="hellorandomuser"
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.post('/sigup',function(req,res){
+    const username=req.body.username
+    const password=req.body.password
+
+    if(!username || !password){
+        return res.json({
+            message:"Username and password fields cannot be empty"
+        })
+    }
+
+    if(username.length<3){
+        return res.json({
+            message:"Username must have at least 3 characters"
+        })
+    }
+    if(users.find((user)=>user.username===username)){
+        return res.json({
+            message:"You are already signned in!"
+        })
+    }
+
+    users.push({
+        username:username,
+        password:password
+    })
+
+    res.json({
+        message:"You have signed up successfully!"
+    })
+})
+
+app.post("/signin",function(req,res){
+    const username=req.body.username
+    const password=req.body.password
+    
+    if(!username || !password){
+        return res.json({
+            message:"Username and password required"
+        })
+    }
+    const foundUser=users.find((user)=>user.username===username && user.password===password)
+
+    if(foundUser){
+        const token=jst.sign({username},JWT_SECRET)
+        res.json({
+            token,message:"You are signned in successfully!"
+        })
+    }else{
+        res.json({
+            message:"Invalid username or password"
+        })
+    }
+})
+
+function auth(req,res,next){
+    const token=req.headers.authorization
+    if(!token){
+        return res.json({
+            message:"No token found"
+        })
+    }
+    try{
+        const decode=jwt.verify(token,JWT_SECRET)
+        req.username=decode.usernamenext()
+    }catch(error){
+        res.json({
+            message:"Invalid token"
+        })
+    }
+}
+
+app.get('/todos',auth,function(req,res){
+    const curruser=req.user
+
+    const usertodo=todos.filter((todo)=>todo.username==curruser)
+
+    res.json(usertodo)
+})
+
+app.post('/todos',auth,function(req,res){
+    const title=req.body.title
+    const curruser=req.body.username
+
+    if(!title){
+        return res.json({
+            message:"Title not found"
+        })
+    }
+    const newTodo={
+        id:todos.length+1,
+        username:curruser,
+        title,
+        done:false
+    }
+    todos.push(newTodo)
+    res.json({
+        message:"Todo created successfully"
+    })
+})
+
+app.put("/todos/:id",auth,function(req,res){
+    const id=req.params.id
+    const title=req.body.title
+    const curruser=req.body.username
+
+    const todo=todos.find((todo)=>todo.id===parseInt(id) && todo.username===curruser)
+
+    if(!title){
+        return res.json({
+            message:"Todo can not be empty"
+        })
+    }
+    todo.title=title
+    res.json({
+        message:"Todo updated successfully"
+    })
+})
+
+app.delete('/todos/:id',auth,function(req,res){
+    const id=req.params.id;
+    const curruser=req.body.username
+
+    const todoIndex=todos.findIndex((todo)=>todo.id===parseInt(id) && todo.username===curruser)
+
+    if(todoIndex===-1){
+        return res.json({
+            message:"Todo bot found"
+        })
+    }
+    todos.splice(todoIndex,1)
+
+    res.jsob({
+        message:"Todo deleted successfully"
+    })
+})
+
+app.post("/todos/:id",auth,function(req,res){
+    const id=req.params.id
+    const curruser=req.body.username
+
+    const todo=todos.find((todo)=>todo.id===parseInt(id) && todo.username===curruser)
+
+    if(!todo){
+        return res.json({
+            message:"Todo not found"
+        })
+    }
+
+    todo.done=!todo.done
+
+    res.json({
+        message:`Todo marked as ${todo.done?"done":"undone"}`,todo
+    })
+})
+
+app.listen(3000)
